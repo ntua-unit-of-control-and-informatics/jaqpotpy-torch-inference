89d1afc33a53089bb64a1ce8b39fde9911549b5b
Giannis Pitoskas <jpitoskas@gmail.com>
2024-06-28 11:50:02 +0300

    refactor: Add additional params per type in doc

--- app/api/v1/handlers/prediction_handler.py
+++ app/api/v1/handlers/prediction_handler.py
@@ -32,6 +32,40 @@ def handle_prediction(model_data: dict, input_values: list) -> List[SinglePredic
 
     Raises:
     - HTTPException(404): If the model type is unknown or unsupported.
+
+    Notes on model_data['additional_model_params']:
+        For each model type, the following additional parameters are expected in the database:
+        
+        TORCH-regression-graph-model:
+            - featurizer (str): The featurizer.
+            - normalization_mean (float): The target's normalization mean.
+            - normalization_std (float): The target's normalization standard deviation.
+        TORCH-binary-graph-model:
+            - featurizer (str): The featurizer.
+            - decision_threshold (float): The decision threshold.
+        TORCH-multiclass-graph-model:
+            - featurizer (str): The featurizer.
+        TORCH-regression-graph-model-with-external:
+            - featurizer (str): The featurizer.
+            - external_preprocessor (str): The external preprocessor.
+            - normalization_mean (float): The target's normalization mean.
+            - normalization_std (float): The target's normalization standard deviation.
+        TORCH-binary-graph-model-with-external:
+            - featurizer (str): The featurizer.
+            - external_preprocessor (str): The external preprocessor.
+            - decision_threshold (float): The decision threshold.
+        TORCH-multiclass-graph-model-with-external:
+            - featurizer (str): The featurizer.
+            - external_preprocessor (str): The external preprocessor.
+        TORCH-binary-fc-model:
+            - preprocessor (str): The preprocessor.
+            - decision_threshold (float): The decision threshold.
+        TORCH-regression-fc-model:
+            - preprocessor (str): The preprocessor.
+            - normalization_mean (float): The target's normalization mean.
+            - normalization_std (float): The target's normalization standard deviation.
+        TORCH-multiclass-fc-model:
+            - preprocessor (str): The preprocessor.
     """
 
     model_type = model_data['type']
